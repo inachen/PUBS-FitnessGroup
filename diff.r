9cc5fc6fc2e103bfbeb6a2ff0d3c888ea95f4ca3
inachen <ina.w.chen@gmail.com>
2014-11-03 15:30:53 -0800

    updated

--- diff.r
+++ diff.r
@@ -4,22 +4,34 @@ compare_dats <- function(dat1, dat2, err1, err2){
     diff = dat1 - dat2
     diff_err = sqrt(err1^2 + err2^2)
 
-    library("pheatmap")
+    # library("pheatmap")
 
     # heatmaps of differences
     # change cluster_cols = TRUE to cluster by position
-    pheatmap(diff, cluster_rows=FALSE, cluster_cols=FALSE, main="Difference Heatmap", show_colnames=TRUE )
-    pheatmap(diff_err, cluster_rows=FALSE, cluster_cols=FALSE, main="Error Heatmap", show_colnames=TRUE )
+    # pheatmap(diff, cluster_rows=FALSE, cluster_cols=FALSE, main="Difference Heatmap", show_colnames=TRUE )
+    # pheatmap(diff_err, cluster_rows=FALSE, cluster_cols=FALSE, main="Error Heatmap", show_colnames=TRUE )
+
+
+    return diff
 }
 
-calc_scores <- function(dat1, dat2, err1, err2){
+calc_wald <- function(dmat, err, alpha){
 
-    # TO-DO!
-    # check out t-tests and what not that r have functions for
+    # calculate wald scores for two matrices 
+    p = 0
+
+    # W <- (dmat-p)^2 / (dmat*(1-dmat)/n)
+    W <- (dmat-p)^2 / err
+    pval = 1-pchisq(W, df=1)    # p-value 
 
 }
 
-run <- function(){
+compress_to_pos <- function(mat){
+
+    return colMeans(mat, na.rm=TRUE)
+}
+
+run <- function(mat){
     # main run function
 
     # load data
@@ -29,12 +41,23 @@ run <- function(){
 
     # if err values given in different files
     # may just need to extract err values from the data csv's
-    err1 = read.csv("data_files/err1.csv", row.names=1, header=TRUE, check.names=FALSE)
-    err2 = read.csv("data_files/err2.csv", row.names=1, header=TRUE, check.names=FALSE)
+    err_mat = read.csv("data_files/err.csv", row.names=1, header=TRUE, check.names=FALSE)
 
     # draw heatmaps
-    compare_dats(dat1, dat2, err1, err2)
+    diff_mat = compare_dats(dat1, dat2, err1, err2)
+
+    # get diff by position
+    diff_lst = compress_to_pos(diff_mat)
 
     # do stat tests
-    calc_scores(dat1, dat2, err1, err2)
+    alpha = 0.05
+    wald_mat = calc_wald(diff_mat, err_mat, alpha)
+
+    # get wald scores by pos
+    wald_lst = compress_to_pos(wald_mat)
 }
+
+run()
+
+
+
