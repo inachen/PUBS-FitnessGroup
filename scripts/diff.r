bcee0ded7969c5af10945ea48055d6cf9fc074da
ichen <ina.chen@ucsf.edu>
2014-11-10 02:21:57 -0800

    finished diff.r

--- scripts/diff.r
+++ scripts/diff.r
@@ -1,26 +1,39 @@
-compare_dats <- function(dat1, dat2, err1, err2){
-    # calculates difference and graphs heatmap
+# CONSTANTS
 
+# data files directory 
+# make sure these directories exist
+FDIR = "data_files"
+OUTDIR = "out_files"
+
+# separation for file names
+FSEP = "/"
+
+# Wald test significance value
+ALPHA = 0.05
+
+compare_dats <- function(dat1, dat2){
+
+    '''Calculate the difference matrix'''
+
+    # calculates difference matrices
     diff = dat1 - dat2
-    diff_err = sqrt(err1^2 + err2^2)
-
-    # library("pheatmap")
-
-    # heatmaps of differences
-    # change cluster_cols = TRUE to cluster by position
-    # pheatmap(diff, cluster_rows=FALSE, cluster_cols=FALSE, main="Difference Heatmap", show_colnames=TRUE )
-    # pheatmap(diff_err, cluster_rows=FALSE, cluster_cols=FALSE, main="Error Heatmap", show_colnames=TRUE )
-
+    # diff_err = sqrt(err1^2 + err2^2)
 
     return diff
+    # return diff_err
 }
 
 calc_wald <- function(dmat, err, alpha){
 
-    # calculate wald scores for two matrices 
+    '''Calculate matrix of Wald values given values and errors'''
+
+    # null hypothesis
     p = 0
 
-    # W <- (dmat-p)^2 / (dmat*(1-dmat)/n)
+    # if no error matrix given
+    # W <- (dmat-p)^2 / (dmat*(1-dmat)/n) 
+
+    # calculate score
     W <- (dmat-p)^2 / err
     pval = 1-pchisq(W, df=1)    # p-value 
 
@@ -28,36 +41,69 @@ calc_wald <- function(dmat, err, alpha){
 
 compress_to_pos <- function(mat){
 
+    '''compress to array at position'''
+
     return colMeans(mat, na.rm=TRUE)
 }
 
-run <- function(mat){
-    # main run function
+to_csv <- function(fname){
 
-    # load data
-    # change path and parameters as needed
-    dat1 = read.csv("data_files/fitness1.csv", row.names=1, header=TRUE, check.names=FALSE)
-    dat2 = read.csv("data_files/fitness2.csv", row.names=1, header=TRUE, check.names=FALSE)
+    return paste(fname, 'csv', sep='.')
+}
 
-    # if err values given in different files
-    # may just need to extract err values from the data csv's
-    err_mat = read.csv("data_files/err.csv", row.names=1, header=TRUE, check.names=FALSE)
+run <- function(fname1, fname2, err){
 
-    # draw heatmaps
-    diff_mat = compare_dats(dat1, dat2, err1, err2)
+    # load data (no row or column names)
+    dat1 = read.csv(paste(FDIR, to_csv(fname1), sep=FSEP), header=FALSE, check.names=FALSE) # row.names=1
+    dat2 = read.csv(paste(FDIR, to_csv(fname2), sep=FSEP), header=FALSE, check.names=FALSE)
+
+    err_mat = read.csv(paste(FDIR, err, sep=FSEP), header=FALSE, check.names=FALSE)
+
+    # get difference matrix
+    diff_mat = compare_dats(dat1, dat2)
 
     # get diff by position
     diff_lst = compress_to_pos(diff_mat)
 
     # do stat tests
-    alpha = 0.05
-    wald_mat = calc_wald(diff_mat, err_mat, alpha)
+    wald_mat = calc_wald(diff_mat, err_mat, ALPHA)
 
     # get wald scores by pos
     wald_lst = compress_to_pos(wald_mat)
+
+    # write results to matrix
+    write.matrix(diff_mat, file = paste(OUTDIR, to_csv(paste(fname1, fname2, 'diff_mat', '_')), sep="/"), sep=",")
+    write.matrix(diff_lst, file = paste(OUTDIR, to_csv(paste(fname1, fname2, 'diff_lst', '_')), sep="/"), sep=",")
+    write.matrix(wald_mat, file = paste(OUTDIR, to_csv(paste(fname1, fname2, 'wald_mat', '_')), sep="/"), sep=",")
+    write.matrix(wald_lst, file = paste(OUTDIR, to_csv(paste(fname1, fname2, 'wald_lst', '_')), sep="/"), sep=",")
+
+
+    #########################
+    # IF PLOTTING, UNCOMMENT
+    #########################
+    # heatmaps of differences
+    # change cluster_cols = TRUE to cluster by position
+
+    # library("pheatmap")
+    # pheatmap(diff_mat, cluster_rows=FALSE, cluster_cols=FALSE, main="Difference Heatmap", show_colnames=TRUE )
+
+    # histogram of difference by position
+
+    # hist(diff_lst)
+
+    # heatmaps of alpha values
+
+    # library("pheatmap")
+    # pheatmap(wald_mat, cluster_rows=FALSE, cluster_cols=FALSE, main="Difference Heatmap", show_colnames=TRUE )
+
+    # histogram of wald test score by position
+
+    # hist(wald_lst)
+
+
 }
 
-run()
+run("fitness1", "fitness2", "err")
 
 
 
